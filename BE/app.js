e01e29a0bd2804b068f8a2e89d699373ad329378
Yuliana R <iuliana.rotaru234@gmail.com>
2026-06-22 17:42:19 +0100

    Update app.js to allow access to views and local deployment

--- BE/app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const router = require("./routers");
-
-app.use("/", router);
-
-module.exports = app;
---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = require("./BE/routers.js");
+const port = process.env.PORT || 3000;
+
+// view engine setup
+app.set("views", path.join(__dirname, "FE/views"));
+app.set("view engine", "ejs");
+
+// static files & body parsing
+app.use(express.static(path.join(__dirname, "FE")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+// 404 handler
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .render("404", { url: req.originalUrl, title: "Page not found" });
+});
+
+// start server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
