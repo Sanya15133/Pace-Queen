ee1c6845aeba42d03b61468cf3cda978312895bd
Sanya Hussain <sanz_133@hotmail.com>
2026-06-22 09:38:02 +0100

    all routes completed

--- BE/routers.js
+++ BE/routers.js
@@ -4,8 +4,25 @@ const {
   getActivityById,
   getActivityByCategoryID,
 } = require("./controllers/activity-controller");
-const router = express.router();
+const {
+  registerUser,
+  logInUser,
+  getUsers,
+  getUserById,
+} = require("./controllers/user-controller");
+const {
+  getCategories,
+  getCategoryById,
+} = require("./controllers/category-controller");
+const router = express.Router();
 
 router.get("/activites", getActivities);
 router.get("/activities/:id", getActivityById);
 router.get("/activities/categories/:id", getActivitiesByCategoryID);
+router.get("/register", registerUser);
+router.get("/login", logInUser);
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.get("/categories", getCategories);
+router.get("/categories/:id", getCategoryById);
+router.get("/categories/activities/:id", getActivityByCategoryID);
