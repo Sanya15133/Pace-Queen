0e97acd27b14bc8baa8199d3246d5691258913d0
Sanya Hussain <sanz_133@hotmail.com>
2026-06-24 10:18:21 +0100

    started front end api calls

---
+++ FE/main.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+const registerUser = async ({ username, email, password, cycle }) => {
+  try {
+    const { data } = await api.post("/users", {
+      username,
+      email,
+      password,
+      cycle,
+    });
+    return data;
+  } catch (err) {
+    console.log(err, "err");
+  }
+};
+
+const getUser = async (id) => {
+  try {
+    const { data } = await api.get("/users", {
+      params: { id },
+    });
+    if (!data) {
+      console.log("This user does not exist");
+    }
+    return data;
+  } catch (err) {
+    console.log(err, "error");
+  }
+};
